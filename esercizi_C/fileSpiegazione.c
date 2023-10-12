e5aec7cd23d437a99b5410c65c805e7d8b979e3a
David Negrello <david.negrello@iisviolamarchesnini.edu.it>
2023-10-12 09:25:28 +0200

    nuovo file

---
+++ esercizi_C/fileSpiegazione.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+int main(int argc, char *argv[])
+{
+    //portare dati da un array a un file esterno
+
+    //1) variabile puntatore a file 
+    FILE *portaFile; //scrittura, lettura
+    portaFile=fopen
+
+    return 0;
+}
