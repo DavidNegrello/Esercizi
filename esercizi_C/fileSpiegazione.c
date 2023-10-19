7daaef1cb9689ceda758ef73f735123c10257fd2
David Negrello <david.negrello@iisviolamarchesnini.edu.it>
2023-10-19 09:24:57 +0200

    modifiche

--- esercizi_C/fileSpiegazione.c
+++ esercizi_C/fileSpiegazione.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
-
-
+//portare dati da un array a un file esterno
+//scrivere in un file
 int main(int argc, char *argv[])
 {
-    //portare dati da un array a un file esterno
-
+    char c;
+    char vetNomi[20];
     //1) variabile puntatore a file 
     FILE *portaFile; //scrittura, lettura
-    portaFile=fopen;
-
+    portaFile=fopen("nomi.txt","w"); //apre il file in uno .txt o lo crea se non esiste
+    if(portaFile==NULL) //se il file non riesce ad aprirlo
+    {
+        printf("File non aperto");
+        exit (1);
+    }
+    do
+    {
+        c=fgetc(portaFile); //prende il primo carattere che trova e lo mette dentro a porta file
+        putchar(c); //stampa il carattere
+    } while (c!=EOF);   //finchè non finiscono i caratteri, significa "end of file"
+    
+    for(int i=0;i<4;i++)
+    {
+        printf("inserisci il nome %d\n",i+1);
+        scanf("%s\n",vetNomi);
+        fputs(vetNomi,portaFile);  //put della stringa dentro il file .txt
+        fputc('\n',portaFile);  //aggiunge il carattere
+    }
+    fclose(portaFile);
     return 0;
 }
