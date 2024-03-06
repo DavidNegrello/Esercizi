914253c74b527d9770d632ae915466c990f6b8a7
David <david.negrello@iisviolamarchesini.edu.it>
2024-03-06 16:49:37 +0100

    esercizio chat

---
+++ esercizi_C/esercizi/Esercizi_Pipe-FIfo/Chat/persona1.c
@@ -0,0 +1,46 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+    int fifo_w;
+    int fifo_r;
+    char frase_scritta[1024];
+    char frase_letta[1024];
+    fifo_w = open("polesella", O_WRONLY);
+    printf("La fifo è stata aperta correttamente\n");
+    fifo_r = open("polesella2", O_RDONLY);
+    printf("La fifo è stata aperta correttamente\n");
+    
+    printf("Persona 1, inserisci una frase: \n");
+    scanf("%s",frase_scritta);
+    do
+    {
+        if (write(fifo_w, frase_scritta, sizeof(frase_scritta)) == -1)
+        {
+            printf("Errore scrittura nella fifo\n");
+            return 2;
+        }
+         if (read(fifo_r, frase_letta, sizeof(frase_letta) == -1))
+        {
+            printf("Errore scrittura nella fifo\n");
+            return 2;
+        }
+        printf("Persona 2: %s\n",frase_letta);
+
+        printf("Persona 1, inserisci una frase: \n");
+        scanf("%s",frase_scritta);
+    } while (strcmp(frase_scritta,"halt"));
+
+    close(fifo_w);
+    close(fifo_r);
+
+    return 0;
+}
---
+++ esercizi_C/esercizi/Esercizi_Pipe-FIfo/Chat/persona2.c
@@ -0,0 +1,51 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+    
+    int fifo_w;
+    int fifo_r;
+    char frase_scritta[1024];
+    char frase_letta[1024];
+    fifo_w = open("polesella2", O_WRONLY);
+    printf("La fifo è stata aperta correttamente\n");
+    fifo_r = open("polesella", O_RDONLY);
+    printf("La fifo è stata aperta correttamente\n");
+    printf("Persona 2, inserisci una frase: \n");
+    scanf("%s",frase_scritta);
+    do
+    {
+        if (read(fifo_r, frase_letta, sizeof(frase_letta) == -1))
+        {
+            printf("Errore scrittura nella fifo\n");
+            return 2;
+        }
+        printf("Persona 1: %s\n",frase_letta);
+
+        printf("Persona 2, inserisci una frase: \n");
+        scanf("%s",frase_scritta);
+        
+        if (write(fifo_w, frase_scritta, sizeof(frase_scritta)) == -1)
+        {
+            printf("Errore scrittura nella fifo\n");
+            return 2;
+        }
+    } while (strcmp(frase_scritta,"halt"));
+    
+    
+
+
+    printf("Scittura eseguita nella fifo\n");
+    close(fifo_w);
+    close(fifo_r);
+
+    return 0;
+}
