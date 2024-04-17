4402232ee889f1c578d8884bb4ebf31176b6927c
David <david.negrello@iisviolamarchesini.edu.it>
2024-04-17 15:32:04 +0200

    esercizi_thread

---
+++ esercizi_C/esercizi/thread/esArray.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+int vettore[10] = {0, 1, 2, 3, 4, 5, 8, 6, 7, 9};
+
+void *RicercaPrima(void *par)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (vettore[i] == 8)
+        {
+            printf("Posizione prima: %d\n", i);
+        }
+    }
+
+    return (void *)0;
+}
+void *RicercaDopo(void *par)
+{
+    for (int i = 5; i <= 9; i++)
+    {
+        if (vettore[i] == 8)
+        {
+            printf("Posizione dopo: %d\n", i);
+        }
+    }
+
+    return (void *)0;
+}
+int main(int argc, char *argv[])
+{
+    
+    pthread_t prima, dopo;
+
+    
+
+    printf("Polesella\n");
+    pthread_create(&prima, NULL, &RicercaPrima, NULL);
+    pthread_create(&dopo, NULL, &RicercaDopo, NULL);
+
+    pthread_join(prima, NULL);
+    pthread_join(dopo, NULL);
+
+    return 0;
+}
---
+++ esercizi_C/esercizi/thread/esStudente.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+
+typedef struct {
+    char nome[50];
+    char cognome[50];
+    char classe[10];
+    float media;
+}Studente;
+
+
+void *printStudentData(void *arg) {
+     Studente *studente = (Studente *) arg;
+
+    printf("Student data:\n");
+    printf("Name: %s\n", studente->nome);
+    printf("Surname: %s\n", studente->cognome);
+    printf("Class: %s\n", studente->classe);
+    printf("Grade: %.2f\n", studente->media);
+
+    pthread_exit(0);
+}
+
+
+void *ScritturaFile(void *arg) {
+
+    Studente *studente = (Studente *) arg;
+    FILE *file;
+
+    file = fopen("dati_studente.txt", "w");
+    if (file == NULL) {
+        printf("Errore apertura file\n");
+        pthread_exit(0);
+    }
+
+    fprintf(file, "Student data:\n");
+    fprintf(file, "Name: %s\n", studente->nome);
+    fprintf(file, "Surname: %s\n", studente->cognome);
+    fprintf(file, "Class: %s\n", studente->classe);
+    fprintf(file, "Grade: %.2f\n", studente->media);
+
+    fclose(file);
+    pthread_exit(0);
+}
+
+int main() {
+    Studente studente;
+    pthread_t stampa, scrittura;
+
+    printf("Inserimento dati dello studente:\n");
+    printf("Nome: ");
+    scanf("%s", studente.nome);
+    printf("Cognome: ");
+    scanf("%s", studente.cognome);
+    printf("Classe: ");
+    scanf("%s", studente.classe);
+    printf("Media: ");
+    scanf("%f", &studente.media);
+
+    
+    pthread_create(&stampa, NULL, printStudentData, &studente);
+    pthread_create(&scrittura, NULL, ScritturaFile, &studente);
+
+    
+    pthread_join(stampa, NULL);
+    pthread_join(scrittura, NULL);
+
+    return 0;
+}
