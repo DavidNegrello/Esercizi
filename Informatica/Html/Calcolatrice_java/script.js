5bc5dab57d84e1aa5e5669746420324589a9c42c
DavidNegrello <121225931+DavidNegrello@users.noreply.github.com>
2024-06-04 16:47:24 +0200

    Aggiunte nuove condizioni

--- Informatica/Html/Calcolatrice_java/script.js
+++ Informatica/Html/Calcolatrice_java/script.js
@@ -1,4 +1,16 @@
-const bottoni=document.querySelectorAll("button");
-bottoni[0].addEventListener("click",()=>{
-    
-})
+const buttons = document.querySelectorAll('.calculator-grid button');
+const display = document.getElementById('display');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const buttonText = button.textContent;
+      //fa tutti gli inserimenti tranne quelli nella condizione
+      if (buttonText!== '=' && buttonText!== 'Canc') {
+        display.value += buttonText;
+      }
+      //cancella l'input precedente utilizzando le stringhe
+      else if(buttonText=='Canc'){
+        display.value = display.value.substring(0, display.value.length - 1);
+      }
+    });
+  });
