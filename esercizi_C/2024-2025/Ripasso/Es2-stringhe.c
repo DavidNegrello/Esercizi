025f0598b1b17e5ab8d18a6d801996b6e116feef
David <david.negrello@iisviolamarchesini.edu.it>
2024-10-01 15:00:17 +0200

    fatto le stringhe MATTEOOOO

--- esercizi_C/2024-2025/Ripasso/Es2-stringhe.c
+++ esercizi_C/2024-2025/Ripasso/Es2-stringhe.c
@@ -22,50 +22,87 @@ Inserita una 2° stringa determinare:
 4) Quale delle 2 stringhe contiene più consonanti;
 */
 #define DIM 100
-void VerificaStringa(char str1[]){
+void VerificaStringa(char str1[])
+{
     for (int i = 0; i < strlen(str1); i++)
     {
-        if (str1[i]>=48||str1[i]<=57)
+        if (str1[i] >= 48 || str1[i] <= 57)
         {
             printf("Presenza di un numero\n");
             break;
         }
     }
 }
-void ConteggioLettera(char str1[],char lettera){
-    int contatore=0;
-    for (int i = 0; i <strlen(str1); i++)
+void ConteggioLettera(char str1[], char lettera)
+{
+    int contatore = 0;
+    for (int i = 0; i < strlen(str1); i++)
     {
-        if (str1[i]==lettera)
+        if (str1[i] == lettera)
         {
             contatore++;
         }
-        
     }
-    printf("La lettera è presente %d volte",contatore);
+    printf("La lettera è presente %d volte", contatore);
 }
-void ConteggioLettera(char str1[],char strPari[],char strDispari[]){
-    for (int i = 0; i <strlen(str1); i++)
+void ConteggioLettera(char str1[], char strPari[], char strDispari[])
+{
+    for (int i = 0; i < strlen(str1); i++)
     {
-        
+        if (strlen(str1) % 2 == 0)
+        {
+            strPari[i] = str1[i];
+        }
+        else
+        {
+            strDispari[i] = str1[i];
+        }
     }
-    
+}
+void Doppie(char str1[]){
+    for (int i = 0; i < strlen(str1); i++){
+        for (int j = i + 1; j < strlen(str1); j++){
+            if (str1[i] == str1[j]){
+                printf("La lettera %c è presente due volte\n", str1[i]);
+            }
+        }
+    }
+}
+void LungaLunga(char str2[], char str1[])
+{
+    if (strlen(str1) > strlen(str2))
+    {
+        printf("La prima stringa è più lunga");
+    }
+    else if (strlen(str1) < strlen(str2))
+    {
+        printf("La seconda stringa è più lunga");
+    }
+    else
+    {
+        printf("Le due stringhe hanno la stessa lunghezza");
+    }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     char str1[DIM];
+    char str2[DIM];
     char lettera;
     printf("Inserisci una stringa\n");
-    scanf("%s",str1);
+    scanf("%s", str1);
     VerificaStringa(str1);
     printf("Che lettera vuoi contare\n");
-    scanf(" %c",&lettera);  //lo spazio serve per evitare lo \n
-    ConteggioLettera(str1,lettera);
-    //2 stringhe, pari e dispari
+    scanf(" %c", &lettera); // lo spazio serve per evitare lo \n
+    ConteggioLettera(str1, lettera);
+    // 2 stringhe, pari e dispari
     char strPari[DIM];
     char strDispari[DIM];
-    PariDispari(str1,strPari,strDispari);
-
+    PariDispari(str1, strPari, strDispari);
+    //nuova stringa
+    printf("Inserisci la nuova stringa\n");
+    scanf("%s", str2);
+    LungaLunga(str2, str1);
     return 0;
 }
