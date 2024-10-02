0c8efd110ce40191294aa3ce7f3b61d597133aad
David <david.negrello@iisviolamarchesini.edu.it>
2024-10-02 16:59:27 +0200

    fatta la lettura

--- esercizi_C/2024-2025/Es_malposti/Es1_libriCSV/Libreria.c
+++ esercizi_C/2024-2025/Es_malposti/Es1_libriCSV/Libreria.c
@@ -9,8 +9,11 @@
 #include <time.h>
 
 #define DIM 100
-#define Lista 29
-#define buffer_dim  1024 
+#define Lista 39
+#define buffer_dim 1024
+
+FILE *sorgente;
+char buffer[buffer_dim];
 //=================STRUCT===============
 typedef struct
 {
@@ -18,33 +21,47 @@ typedef struct
     char titolo[DIM];
     char autore[DIM];
     int anno;
-    float prezzo
+    float prezzo;
 } Libreria;
 
 //=================FUNZIONI================
 
+int SalvaLibri(Libreria listaLibri[], int contatore)
+{
+    while (fgets(buffer, sizeof(buffer), sorgente) != NULL)
+    {
+        sscanf(buffer, "%99[^,],%99[^,],%f,%d", listaLibri[contatore].titolo, listaLibri[contatore].autore, &listaLibri[contatore].prezzo, &listaLibri[contatore].anno);
+        // Imposta un valore predefinito per la categoria
+        strcpy(listaLibri[contatore].categoria, "default");
+        contatore++;
+    }
+    return contatore;
+}
 
-
+//=================MAIN================
 int main(int argc, char *argv[])
 {
-    Libreria listaLibri[29];
-    FILE *sorgente;
-    char carattere;
-    unsigned char buffer[buffer_dim];
-
-
+    Libreria listaLibri[Lista];
+    int contatore = 0;
     //==========FILE==========
-    sorgente = fopen("Lista dei libri", "r"); 
+    sorgente = fopen("libreria_libri.csv", "r");
     if (sorgente == NULL)
     {
         printf("Errore apertura file");
         exit(1);
     }
-    while (!feof(sorgente)) 
+    contatore = SalvaLibri(listaLibri, contatore);
+    // Stampa i dati letti
+    for (int i = 0; i < contatore; i++)
     {
-        carattere = fread(buffer,1,buffer_dim,sorgente); 
+        printf("%s, %s, %.2f, %d, %s\n",
+               listaLibri[i].titolo,
+               listaLibri[i].autore,
+               listaLibri[i].prezzo,
+               listaLibri[i].anno,
+               listaLibri[i].categoria);
     }
-
     fclose(sorgente);
     return 0;
 }
+//==========FINE FILE==========
