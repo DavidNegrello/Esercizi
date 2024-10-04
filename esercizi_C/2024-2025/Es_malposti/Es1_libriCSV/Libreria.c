ea074b1eb0a23063840ae0ce68c39c5adf30e10a
David <david.negrello@iisviolamarchesini.edu.it>
2024-10-04 18:48:25 +0200

    mancano le rifiniture come spostare i git close

--- esercizi_C/2024-2025/Es_malposti/Es1_libriCSV/Libreria.c
+++ esercizi_C/2024-2025/Es_malposti/Es1_libriCSV/Libreria.c
@@ -13,6 +13,7 @@
 #define buffer_dim 1024
 
 FILE *sorgente;
+FILE *fileOutput;
 char buffer[buffer_dim];
 //=================STRUCT===============
 typedef struct
@@ -37,38 +38,76 @@ int SalvaLibri(Libreria listaLibri[], int contatore)
     }
     return contatore;
 }
-void Menu(){
+void Menu()
+{
     printf("==========MENU==========\n");
     printf("1. Visualizza la lista dei libri\n");
     printf("2. Modifica lista con inserimento categorie\n");
     printf("3. Visualizza per categoria\n");
     printf("4. Esci\n");
 }
-void ControlloScelta(int scelta, int contatore,Libreria listaLibri[]){
+int ControlloScelta(int scelta, int contatore, Libreria listaLibri[])
+{
+    // char Categorie[buffer_dim]; // per salvare i dati delle modifiche
     char categoriaModifica[DIM];
+    char sceltaCategoria[DIM];
     switch (scelta)
     {
     case 1:
         // Stampa i dati letti
-    for (int i = 0; i < contatore; i++)
-    {
-        printf("%s, %s, %.2f, %d, %s\n",
-               listaLibri[i].titolo,
-               listaLibri[i].autore,
-               listaLibri[i].prezzo,
-               listaLibri[i].anno,
-               listaLibri[i].categoria);
-    }   
-        break;
-    case 2: //canbia categoria
         for (int i = 0; i < contatore; i++)
         {
-            printf("Cambia la categoria del libro %s \n",listaLibri[i].titolo);
-            strcpy(listaLibri[i].categoria, scanf(" %s",  categoriaModifica));
+            printf("%s, %s, %.2f, %d, %s\n",
+                   listaLibri[i].titolo,
+                   listaLibri[i].autore,
+                   listaLibri[i].prezzo,
+                   listaLibri[i].anno,
+                   listaLibri[i].categoria);
         }
-        
+        break;
+    case 2: // canbia categoria
+        for (int i = 0; i < contatore; i++)
+        {
+            printf("Cambia la categoria del libro %s \n", listaLibri[i].titolo);
+            scanf(" %s", categoriaModifica);
+            // strcat(categoriaModifica, "\n");         // Aggiungi una nuova riga alla fine
+            // strcpy(Categorie[i], categoriaModifica); // Copia la stringa con il newline nell'array
+
+            // Dopo la modifica, salva su file
+            ScriviSuFile(listaLibri, contatore);
+        }
+        break;
+    case 3:
+        printf("Scegli una categoria:\n");
+        scanf(" %s", sceltaCategoria);
+        for (int i = 0; i < contatore; i++)
+        {
+            if (strcmp(sceltaCategoria, listaLibri[i].categoria) == 0)
+            {
+                printf("%d %s\n", i + 1, listaLibri[i].categoria);
+            }
+        }
         break;
     }
+    return scelta;
+}
+
+// Funzione per salvare i dati aggiornati in un file
+void ScriviSuFile(Libreria listaLibri[], int contatore)
+{
+
+    for (int i = 0; i < contatore; i++)
+    {
+        // Scrivi ogni libro con le categorie aggiornate
+        fprintf(fileOutput, "%s,%s,%.2f,%d,%s\n",
+                listaLibri[i].titolo,
+                listaLibri[i].autore,
+                listaLibri[i].prezzo,
+                listaLibri[i].anno,
+                listaLibri[i].categoria);
+    }
+
+    fclose(fileOutput);
 }
 //=================MAIN================
 int main(int argc, char *argv[])
@@ -83,22 +122,24 @@ int main(int argc, char *argv[])
         printf("Errore apertura file");
         exit(1);
     }
+    fileOutput = fopen("libreria_libri_modificata.csv", "w");
+    if (fileOutput == NULL)
+    {
+        printf("Errore apertura file per scrittura.\n");
+        return;
+    }
     contatore = SalvaLibri(listaLibri, contatore);
 
-    
+    //==========Menu==========
     do
     {
-        //==========Menu==========
+        
         Menu();
         printf("Inserisci un numero come scelta\n");
-        scanf(" %d",&scelta);
-       scelta=ControlloScelta(scelta,contatore,listaLibri);
-    } while (scelta!=4);
-    
+        scanf(" %d", &scelta);
+        scelta = ControlloScelta(scelta, contatore, listaLibri);
+    } while (scelta != 4);
 
-
-
-    
     fclose(sorgente);
     return 0;
 }
