6da5970b8f0fa1801f2b4fde287284db31a843a3
DavidNegrello <david.negrello@iisviolamarchesini.edu.it>
2024-11-28 17:25:28 +0100

    sistemato il login
    sistemato il login tranne quando bisogna sloggare dalla home

--- esercizi_C/2024-2025/Frontend/Socket e Bootstrap 5/js/login.js
+++ esercizi_C/2024-2025/Frontend/Socket e Bootstrap 5/js/login.js
@@ -6,15 +6,21 @@ function handleLogin(event) {
     // Salva il nome utente nel localStorage per mantenerlo disponibile anche dopo il riavvio della pagina
     localStorage.setItem("username", username);
     // Reindirizza l'utente alla pagina principale (home) dopo che il login è stato eseguito
-    window.location.href = "index.html"; 
+    window.location.href = "../index.html"; 
 }
 
 
 // Funzione per gestire l'accesso anonimo
 function handleGuestAccess() {
+    // Mostra una finestra di prompt chiedendo all'utente di inserire un nome per accedere come ospite
     const guestName = prompt("Inserisci un nome per accedere come ospite:", "Ospite");
+
+    // Verifica che l'utente abbia inserito un nome (cioè che guestName non sia vuoto o nullo)
     if (guestName) {
+        // Salva il nome dell'ospite nel localStorage con la chiave "username"
         localStorage.setItem("username", guestName);
-        window.location.href = "index.html"; // Reindirizza alla home
+
+        // Reindirizza l'utente alla home page (index.html)
+        window.location.href = "../index.html"; // Reindirizza alla home
     }
 }
